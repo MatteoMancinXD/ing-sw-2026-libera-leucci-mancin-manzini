93559831dee10fbbe926946b9d8f7c99d0435cb1
Riccardo Libera <riccardo.libera@mail.polimi.it>
2026-04-13 20:25:24 +0200

    Update  RitualEvent.java using the new methods

--- Mesos/src/main/java/it/polimi/ingsw/model/events/RitualEvent.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/events/RitualEvent.java
@@ -3,56 +3,64 @@ package it.polimi.ingsw.model;
 
 import java.util.ArrayList;
 
-/*
-* da class Game serve
-*   public ArrayList<Player> getPlayers(){ // (Game game) come parametro se ci dovessero essere piu game
- *      ArrayList<Player> playersInGame = players;
- *      return playersInGame;
-* */
-
-
-
 public class RitualEvent extends EventCard{
-    public solveEventCard(Player player,Era era){
+    @Override
+    public void solveEventCard(Game game,Player player){
 
-        ArrayList<Player> playersInGame = getPlayers(); //or game.getPlayers(); if there's more than 1 game
-        ArrayList<Card> playerDeck = player.getPlayerCards();
+        boolean buildingBonusI = false;
+        boolean buildingBonusII = false; 
+
+        int era = this.getEra();
 
 
-
-        // !!! goes into CONTROLLER and then PUT A METHOD AND CALL IT HERE
-        //compare the player's stars with the other players' stars
+        //compare the player's stars with the other players' stars: if the player's is among the bests or among the worsts: then give bonus/malus
         int i=0;
         int j=0;
+        
+        for(Player playerInGame : game.getPlayersView()){ //game.getPlayersView is the unmodifiable list of players inside the game
+            if(!(playerInGame.equals(player))){
+                if(player.getTotStars() >= playerInGame.getTotStars()){
+                    i++;
+                }
+                if (player.getTotStars() <= playerInGame.getTotStars()) {
+                    j++;
+                }
+            }
 
-        for(Player playerInGame : playersInGame && !playerInGame.equals(player)){
-            if(player.getTotStars() >= playerInGame.getTotStars()){
-                i++;
-            } else if (player.getTotStars() <= playerInGame.getTotStars()) {
-                j++;
+        }
+
+
+        
+        for(BuildingCard buildingCard : player.getBuildings()){
+            if(buildingCard instanceof RitualEventBuildingII){
+                buildingBonusII = true; 
+                break; 
             }
         }
-        // !!!
-
-
+        for(BuildingCard buildingCard : player.getBuildings()){
+            if(buildingCard instanceof RitualEventBuildingI){
+                buildingBonusI = true; 
+                break; 
+            }
+        }
 
         //i and j are to be removed with the controller's "stars compare method"
-        if(i==playersInGame.size()-1){  //all the others - himself(1)
+        if(i==game.getNumPlayers()-1){  //all the others - himself(1)
             //player who invoked the solve event method has more stars than all the others (or equal to someone else with whom has more stars than all the others)
             switch(era){
                 case(1):
-                    player.editPrestige(5 * (player.CardInDeck(RitualEventBuildingII) ? 2 : 1));
+                    player.editPrestige(5 * (buildingBonusII ? 2 : 1));
                     break;
                 case(2):
-                    player.editPrestige(10 * (player.CardInDeck(RitualEventBuildingII) ? 2 : 1));
+                    player.editPrestige(10 * (buildingBonusII ? 2 : 1));
                     break;
                 case(3):
-                    player.editPrestige(15 * (player.CardInDeck(RitualEventBuildingII) ? 2 : 1));
+                    player.editPrestige(15 * (buildingBonusII ? 2 : 1));
                     break;
             }
         }
         //if player has the RitualEventBuildingI no malus
-        if(( j==playersInGame.size()-1) && !player.CardInDeck(RitualEventBuildingI)){
+        if(( j==game.getNumPlayers()-1) && !buildingBonusI){
             //player who invoked the solve event method has less stars than all the others (or equal to someone else with whom has less stars than all the others)
             switch(era){
                 case(1):
