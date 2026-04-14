08969d873c2bb1a865af2f556e1fdf21b0c3e188
RiccardoLibera <riccardo.libera@mail.polimi.it>
2026-04-14 15:36:24 +0200

    some event buildings added

---
+++ Mesos/src/main/java/it/polimi/ingsw/model/buildings/HuntEventBuilding.java
@@ -0,0 +1,12 @@
+package it.polimi.ingsw.model;
+
+public class HuntEventBuilding extends BuildingCard {
+
+    public HuntEventBuilding() {
+        super(2, 7, 2); // era, foodCost, prestigeGain
+    }
+
+    //During HuntEvent gives an extra +1 prestige +1 food for each Hunter
+    //check happens in class HuntEvent
+
+}
---
+++ Mesos/src/main/java/it/polimi/ingsw/model/buildings/RitualEventBuildingI.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw.model;
+
+public class RitualEventBuildingI extends BuildingCard {
+
+    public RitualEventBuildingI() {
+        super(1,5,2);
+    }
+
+    //check happens inside RitualEvent class
+}
