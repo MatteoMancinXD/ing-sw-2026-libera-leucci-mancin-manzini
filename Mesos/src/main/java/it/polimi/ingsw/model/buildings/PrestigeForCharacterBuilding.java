84f0331c2400631a34a5d6be899ea7487b8cb67d
RiccardoLibera <riccardo.libera@mail.polimi.it>
2026-04-18 21:44:14 +0200

    class PrestigeForCharacterBuilding removed checks

--- Mesos/src/main/java/it/polimi/ingsw/model/buildings/PrestigeForCharacterBuilding.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/buildings/PrestigeForCharacterBuilding.java
@@ -12,17 +12,9 @@ public class PrestigeForCharacterBuilding extends BuildingCard {
     private Character character;
     private int bonusPrestige;
 
-    //it works also if two different players draw the cards, because it s a static set, and because it s hash it can contain nothing
-    private static final HashSet<Character> TAKEN_CHARACTERS = new HashSet<>();
 
     public PrestigeForCharacterBuilding(int id, int era, int foodCost, int prestigeGain, Character character, int bonusPrestige) {
         super(id,era,foodCost,prestigeGain);
-
-        //checks if the character is already taken (you can declare 1 building per character)
-        if (TAKEN_CHARACTERS.contains(character)) {
-            throw new IllegalStateException("Sustenance building already exists for" + character);
-        }
-        TAKEN_CHARACTERS.add(character); //if it wasn't taken add it to the taken list
 
         this.character=character;
         this.bonusPrestige=bonusPrestige;
@@ -40,9 +32,6 @@ public class PrestigeForCharacterBuilding extends BuildingCard {
 
         player.editPrestige(this.bonusPrestige*(player.getCharacterDeck(this.character).size()));
 
-
-        //on game end after editing prestige,it clears TAKEN_CHARACTERS to free the buildings for next game
-        TAKEN_CHARACTERS.clear();
     }
 
 }
--- Mesos/src/main/java/it/polimi/ingsw/model/buildings/SustenanceBuilding.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/buildings/SustenanceBuilding.java
@@ -5,7 +5,7 @@ import it.polimi.ingsw.model.Player;
 
 import java.util.EnumSet;
 import java.util.Set;
-import java.util.HashSet;
+
 
 
 public class SustenanceBuilding extends BuildingCard{
@@ -21,8 +21,7 @@ public class SustenanceBuilding extends BuildingCard{
                 Character.HARVESTER,
                 Character.INVENTOR
     );
-    //it works also if two different players draw the cards, because it s a static set, and because it s hash it can contain nothing
-    private static final HashSet<Character> TAKEN_CHARACTERS = new HashSet<>();
+
 
 
 
@@ -35,11 +34,7 @@ public class SustenanceBuilding extends BuildingCard{
         if(!ALLOWED_CHARACTERS.contains(character)){
             throw new IllegalArgumentException("Error: " + character + " not a valid Character for SustenanceBuilding");
         }
-        //checks if the character is already taken (you can declare 1 building per character)
-        if (TAKEN_CHARACTERS.contains(character)) {
-            throw new IllegalStateException("Sustenance building already exists for" + character);
-        }
-        TAKEN_CHARACTERS.add(character); //if it wasn't taken add it to the taken list
+
 
 
 
@@ -57,10 +52,6 @@ public class SustenanceBuilding extends BuildingCard{
     }
 
 
-    //check method
-    //MUST BE USED ON endGame()
-    public static void resetTakenSustenanceBuildingCharactersSet() {
-        TAKEN_CHARACTERS.clear();
-    }
+
 
 }
