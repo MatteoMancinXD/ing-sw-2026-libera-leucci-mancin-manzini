3720525ac97d6aee318713919a1d56fc209ea02f
RiccardoLibera <riccardo.libera@mail.polimi.it>
2026-04-18 21:02:56 +0200

    class PrestigeForCharacterBuilding now has CharacterCard as parameter

--- Mesos/src/main/java/it/polimi/ingsw/model/buildings/PrestigeForCharacterBuilding.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/buildings/PrestigeForCharacterBuilding.java
@@ -5,16 +5,38 @@ import it.polimi.ingsw.model.CharacterCard;
 import it.polimi.ingsw.model.Player;
 import it.polimi.ingsw.model.BuildingCard;
 
+import java.util.HashSet;
+
 public class PrestigeForCharacterBuilding extends BuildingCard {
-    public PrestigeForCharacterBuilding(int id, int era, int foodCost, int prestigeGain) {
+    private CharacterCard characterCard;
+
+    //it works also if two different players draw the cards, because it s a static set, and because it s hash it can contain nothing
+    private static final HashSet<Character> TAKEN_CHARACTERS = new HashSet<>();
+
+    public PrestigeForCharacterBuilding(int id, int era, int foodCost, int prestigeGain,CharacterCard card) {
         super(id,era,foodCost,prestigeGain);
+
+
+        //checks if the character is already taken (you can declare 1 building per character)
+        if (TAKEN_CHARACTERS.contains(characterCard.getType())) {
+            throw new IllegalStateException("Sustenance building already exists for" + characterCard.getType());
+        }
+        TAKEN_CHARACTERS.add(characterCard.getType()); //if it wasn't taken add it to the taken list
+
+        this.characterCard=card;
+
     }
+
+    //operative methods
+    public void setCharacterCard(CharacterCard characterCard) {this.characterCard=characterCard;}
+    public CharacterCard getCharacterCard() { return this.characterCard; }
+
     @Override
-    public void onCharacterCardGameEnd(Player player, CharacterCard card){
+    public void onGameEnd(Player player){
 
-        int deckSize = player.getCharacterDeck(card.getType()).size();
+        int deckSize = player.getCharacterDeck(characterCard.getType()).size();
 
-        switch(card.getType()){
+        switch(characterCard.getType()){
             case INVENTOR ->  player.editPrestige(deckSize*2);
             case HUNTER ->   player.editPrestige(deckSize*3);
             case HARVESTER,ARTIST,SHAMAN,BUILDER ->   player.editPrestige(deckSize*4);
@@ -22,6 +44,8 @@ public class PrestigeForCharacterBuilding extends BuildingCard {
         }
 
 
+        //on end game after editing the prestige, TAKEN_CHARACTERS is cleaned for the next game
+        TAKEN_CHARACTERS.clear();
 
 
 
