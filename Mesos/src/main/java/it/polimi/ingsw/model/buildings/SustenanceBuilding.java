248b3fb302ede53f0eed3c0397c0ee9dacb4bcbc
Cesare Manzini <manzini.cesare04@gmail.com>
2026-04-22 17:34:44 +0200

    minor updates in SustenanceBuilding

--- Mesos/src/main/java/it/polimi/ingsw/model/buildings/SustenanceBuilding.java
+++ Mesos/src/main/java/it/polimi/ingsw/model/buildings/SustenanceBuilding.java
@@ -10,50 +10,9 @@ import java.util.Set;
 
 public class SustenanceBuilding extends BuildingCard{
 
-    private Character character;
-
-
-    //these collections are needed for checks
-
-    //there are 3 sustenance building each of these work with just one Character type
-    private static final Set<Character> ALLOWED_CHARACTERS = EnumSet.of(
-                Character.ARTIST,
-                Character.HARVESTER,
-                Character.INVENTOR
-    );
-
-
-
-
-    //constructor
-    public SustenanceBuilding(int id, int era, int  foodCost, int prestigeGain, Character character) {
+    public SustenanceBuilding(int id, int era, int  foodCost, int prestigeGain) {
         super(id,era,foodCost,prestigeGain);
-
-
-        //checks if the character is allowed
-        if(!ALLOWED_CHARACTERS.contains(character)){
-            throw new IllegalArgumentException("Error: " + character + " not a valid Character for SustenanceBuilding");
-        }
-
-
-
-
-
-        this.character=character;
     }
 
-    public SustenanceBuilding (){}
-
-    //operative methods
-    public void setCharacter(Character character) {this.character=character;}
-    public Character getCharacter() { return this.character; }
-
-    @Override
-    public int getSustenanceEventFoodBonus(Player p){
-        return p.getCharacterDeck(this.character).size();
-    }
-
-
-
-
+    public SustenanceBuilding () {}
 }
