0f3150b31a01cb2aab2562605c7777bbf7071a66
Cesare Manzini <manzini.cesare04@gmail.com>
2026-04-23 12:31:03 +0200

    InventorSetForFoodBuildingTest implemented

--- Mesos/src/test/java/it/polimi/ingsw/model/PlayerTest.java
+++ Mesos/src/test/java/it/polimi/ingsw/model/PlayerTest.java
@@ -106,45 +106,6 @@ class PlayerTest {
     }
 
     @Test
-    void inventorSetForFoodTest() {
-        Player p = new Player("Player1");
-        BuildingCard bc = new InventorSetForFoodBuilding(1,1,0, 0);
-        CharacterCard inv1 = new InventorCard(2,1,2, Invention.ROPE);
-        CharacterCard inv2 = new InventorCard(3,2,2, Invention.ROPE);
-
-        // Checking that player has 0 food now
-        assertEquals(0, p.getFood());
-
-        p.drawCard(bc);
-        bc.notifyBuildings(p);
-
-        // Checking BuildingDeck
-        assertEquals(1, p.getBuildings().size());
-        assertEquals(bc, p.getBuildings().getFirst());
-
-        p.drawCard(inv1);
-        inv1.notifyBuildings(p);
-
-        // Checking Inventor Deck
-        assertEquals(1, p.getInventors().size());
-        assertEquals(inv1, p.getInventors().getFirst());
-
-        // Checking no food bonus yet
-        assertEquals(0, p.getFood());
-
-        p.drawCard(inv2);
-        inv2.notifyBuildings(p);
-
-        // Checking Inventor Deck
-        assertEquals(2, p.getInventors().size());
-        assertEquals(inv1, p.getInventors().getFirst());
-        assertEquals(inv2, p.getInventors().get(1));
-
-        // Checking food bonus
-        assertEquals(3, p.getFood());
-    }
-
-    @Test
     void noNegativeFoodTest() {
         Player p = new Player("Player1");
 
