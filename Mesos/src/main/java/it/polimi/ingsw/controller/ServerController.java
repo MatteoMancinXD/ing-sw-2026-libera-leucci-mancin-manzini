24867e364c98c44d598c84e239f3750dfeeb7c8f
MatteoMancin <114902631+MatteoMancin@users.noreply.github.com>
2026-04-25 17:31:39 +0200

    Changed virtual views from classes to interfaces

--- Mesos/src/main/java/it/polimi/ingsw/controller/ServerController.java
+++ Mesos/src/main/java/it/polimi/ingsw/controller/ServerController.java
@@ -1,4 +1,18 @@
 package it.polimi.ingsw.controller;
 
+import it.polimi.ingsw.model.Game;
+import it.polimi.ingsw.rmi.VirtualRMIView;
+
+import java.util.List;
+
 public class ServerController {
+
+    private Game game;
+    List<VirtualRMIView> connectedClients;
+
+    public ServerController(List<VirtualRMIView> clients) {
+        this.connectedClients = clients;
+        this.game = new Game(clients.size());
+    }
+
 }
