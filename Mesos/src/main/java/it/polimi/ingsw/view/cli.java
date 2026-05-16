19e5dd4bb9d24a90c9511ba8252bf87d71537131
RiccardoLibera <riccardo.libera@mail.polimi.it>
2026-05-17 00:18:38 +0200

    adjusting after totem-select merge

--- Mesos/src/main/java/it/polimi/ingsw/view/cli.java
+++ Mesos/src/main/java/it/polimi/ingsw/view/cli.java
@@ -9,7 +9,7 @@ import java.sql.SQLOutput;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class cli implements ui{
+public class cli implements ui {
 
     private final Scanner scanner;
     private NetworkClient client;
@@ -20,7 +20,7 @@ public class cli implements ui{
     private List<LeaderboardEntryBean> globalRanks;
     private int numPlayers;
 
-    public cli (String nickname) {
+    public cli(String nickname) {
         this.scanner = new Scanner(System.in);
         this.nickname = nickname;
         this.currentState = CliState.LOBBY;
@@ -33,7 +33,7 @@ public class cli implements ui{
     public void startInputStream() {
         System.out.println("Welcome in MESOS! -help for commands list");
 
-        while(true) {
+        while (true) {
             System.out.print("\n> ");
             String input = scanner.nextLine().trim().toLowerCase();
             if (input.isEmpty()) continue;
@@ -41,7 +41,7 @@ public class cli implements ui{
             String[] parameters = input.split(" ");
             String command = parameters[0];
 
-            switch(currentState) {
+            switch (currentState) {
                 case LOBBY:
                     handleLobbyCommands(command, parameters);
                     break;
@@ -67,14 +67,14 @@ public class cli implements ui{
     }
 
     private void handleTotemCommands(String command, String[] parameters) {
-        switch(command) {
+        switch (command) {
             case "totems":
                 client.requestAvailableTotems();
                 break;
             case "select":
                 String selected = parameters[1].toUpperCase();
                 List<String> stringTotems = Arrays.stream(Totem.values()).map(Enum::name).toList();
-                if(stringTotems.contains(selected)) {
+                if (stringTotems.contains(selected)) {
                     Totem selectedTotem = Totem.valueOf(selected);
                     client.askToSelectTotem(selectedTotem);
                 } else {
@@ -88,6 +88,8 @@ public class cli implements ui{
                 System.out.println("Invalid command");
 
         }
+    }
+
     public void handleEndGameCommands(String command, String[] parameters) {
 
         switch (command) {
@@ -102,55 +104,55 @@ public class cli implements ui{
     }
 
     public void handleLobbyCommands(String command, String[] parameters) {
-           switch(command) {
-               case "create":
-                   try {
-                       try {
-                           System.out.println("Sending game creation request...");
-                           client.createGame(nickname, Integer.parseInt(parameters[1]));
-                       } catch (NumberFormatException e) {
-                           showError(e.getMessage());
-                       }
+        switch (command) {
+            case "create":
+                try {
+                    try {
+                        System.out.println("Sending game creation request...");
+                        client.createGame(nickname, Integer.parseInt(parameters[1]));
+                    } catch (NumberFormatException e) {
+                        showError(e.getMessage());
+                    }
 
-                   } catch (RemoteException e) {
-                       System.out.println("Connection error creating game");
-                   }
-                   break;
-               case "join":
-                   try {
-                       try {
-                           System.out.println("Sending join request...");
-                           client.joinGame(nickname, Integer.parseInt(parameters[1]));
-                       } catch (NumberFormatException e) {
-                           System.out.println("Invalid gameID");
-                       }
+                } catch (RemoteException e) {
+                    System.out.println("Connection error creating game");
+                }
+                break;
+            case "join":
+                try {
+                    try {
+                        System.out.println("Sending join request...");
+                        client.joinGame(nickname, Integer.parseInt(parameters[1]));
+                    } catch (NumberFormatException e) {
+                        System.out.println("Invalid gameID");
+                    }
 
-                   } catch (RemoteException e) {
-                       System.out.println("Connection error joining game");
-                   }
-                   break;
-               case "list":
-                   try {
-                       client.requestAvailableGames();
-                   } catch (RemoteException e) {
-                       showError(e.getMessage());
-                   }
-                    break;
-               case "help":
-                   System.out.println("Lobby commands: create *num_players*  |  join *game_id*  | list ");
-                   break;
-               default:
-                   System.out.println("Invalid command");
-           }
+                } catch (RemoteException e) {
+                    System.out.println("Connection error joining game");
+                }
+                break;
+            case "list":
+                try {
+                    client.requestAvailableGames();
+                } catch (RemoteException e) {
+                    showError(e.getMessage());
+                }
+                break;
+            case "help":
+                System.out.println("Lobby commands: create *num_players*  |  join *game_id*  | list ");
+                break;
+            default:
+                System.out.println("Invalid command");
+        }
     }
 
     public void handleWaitingCommands(String command, String[] parameters) {
-        switch(command) {
+        switch (command) {
             case "chat":
                 StringBuilder message = new StringBuilder();
                 //message.append("[CHAT] ");
-                for(int i = 1; i < parameters.length; i++) {
-                    if(i >= 2) {
+                for (int i = 1; i < parameters.length; i++) {
+                    if (i >= 2) {
                         message.append(" ");
                     }
                     message.append(parameters[i]);
@@ -172,7 +174,7 @@ public class cli implements ui{
     }
 
     public void handleDrawingCommands(String command, String[] parameters) {
-        switch(command) {
+        switch (command) {
             case "help":
                 System.out.println("Drawing commands: draw *row(1:UpperRow, 0:LowerRow)*  *card_index*  |  chat *message*");
                 break;
@@ -182,7 +184,7 @@ public class cli implements ui{
                     row = true;
                 }
                 try {
-                    if(parameters.length == 3) {
+                    if (parameters.length == 3) {
                         System.out.println("Sending draw request...");
                         client.askToDrawCard(row, Integer.parseInt(parameters[2]));
                     } else {
@@ -216,7 +218,7 @@ public class cli implements ui{
 
     public void handlePlacingCommands(String command, String[] parameters) {
 
-        switch(command) {
+        switch (command) {
             case "help":
                 System.out.println("Placing commands:  totem *tile_index*  |  chat *message*");
                 break;
@@ -258,14 +260,13 @@ public class cli implements ui{
         if (currentPlayerNickname.equals(this.nickname)) {
             if (currentGamePhase.equals("PLACEMENT")) {
                 this.currentState = CliState.PLACING;
-            }
-            else if (currentGamePhase.equals("RESOLUTION") || currentGamePhase.equals("EXTRA_PICK")) {
+            } else if (currentGamePhase.equals("RESOLUTION") || currentGamePhase.equals("EXTRA_PICK")) {
                 this.currentState = CliState.DRAWING;
             }
             System.out.println("It's your turn!");
         } else {
             this.currentState = CliState.WAITING;
-            System.out.println("It's " + currentPlayerNickname.toUpperCase()+"'s turn");
+            System.out.println("It's " + currentPlayerNickname.toUpperCase() + "'s turn");
         }
 
         System.out.println("---------------------------------");
@@ -282,11 +283,10 @@ public class cli implements ui{
             if (t.getStatus()) {
                 String player = t.getPlayer().getNickname();
                 status = "Occupied by " + player;
-            }
-            else {
+            } else {
                 status = "is free";
             }
-            System.out.println("Tile " + t.getLetter() + ": "+status+", " + t.getUpperRow() + " upper and " + t.getLowerRow() + " lower");
+            System.out.println("Tile " + t.getLetter() + ": " + status + ", " + t.getUpperRow() + " upper and " + t.getLowerRow() + " lower");
         }
 
         System.out.println("\n");
@@ -307,7 +307,7 @@ public class cli implements ui{
         for (Player p : players) {
             if (p.getNickname().equals(this.nickname)) {
                 System.out.println("\n------YOUR STATS AND CARDS------\n");
-                System.out.println("Food: "+p.getFood()+", Prestige: "+p.getPrestige()+", Your cards: \n");
+                System.out.println("Food: " + p.getFood() + ", Prestige: " + p.getPrestige() + ", Your cards: \n");
                 List<Card> everyCard = new ArrayList<>();
                 everyCard.addAll(p.getArtists());
                 everyCard.addAll(p.getBuilders());
@@ -317,7 +317,7 @@ public class cli implements ui{
                 everyCard.addAll(p.getInventors());
                 everyCard.addAll(p.getBuildings());
                 for (Card c : everyCard) {
-                    System.out.print(c.getShortString()+",  ");
+                    System.out.print(c.getShortString() + ",  ");
                 }
                 System.out.println("\n\n");
             }
@@ -325,8 +325,8 @@ public class cli implements ui{
         System.out.println("------OPPONENTS STATS AND CARDS------\n");
         for (Player p : players) {      //due cicli diversi per carte personali e stats di altri per printare prima le proprie carte sempre
             if (!p.getNickname().equals(this.nickname)) {
-                System.out.println("Nickname: "+p.getNickname()+", Food: "+p.getFood()+", Prestige: "+p.getPrestige());
-                System.out.println("Artists: "+p.getArtists().size() +", Builder: "+p.getBuilders().size()+", Harvesters: "+p.getHarvesters().size()+", Hunters: "+p.getHunters().size()+", Inventors: "+p.getInventors().size()+", Shamans: "+p.getShamans().size()+"\n");
+                System.out.println("Nickname: " + p.getNickname() + ", Food: " + p.getFood() + ", Prestige: " + p.getPrestige());
+                System.out.println("Artists: " + p.getArtists().size() + ", Builder: " + p.getBuilders().size() + ", Harvesters: " + p.getHarvesters().size() + ", Hunters: " + p.getHunters().size() + ", Inventors: " + p.getInventors().size() + ", Shamans: " + p.getShamans().size() + "\n");
             }
         }
         //System.out.println("---------------------------------");
@@ -338,7 +338,7 @@ public class cli implements ui{
     public void showError(String errorMsg) {
         final String ANSI_RED = "\u001B[31m";       //stampa l'errore in rosso, poi resetta a bianco
         final String ANSI_RESET = "\u001B[0m";
-        System.out.println("\n"+ANSI_RED+errorMsg+ANSI_RESET);
+        System.out.println("\n" + ANSI_RED + errorMsg + ANSI_RESET);
         System.out.println("---------------------------------");
         System.out.print("> ");
     }
@@ -350,14 +350,14 @@ public class cli implements ui{
         this.numPlayers = rankings.size();
         System.out.println("\n\n======GAME OVER======");
         System.out.println("Standings: ");
-        for(int i = 0; i < rankings.size(); i++) {
-            System.out.println((i+1)+")"+rankings.get(i));
+        for (int i = 0; i < rankings.size(); i++) {
+            System.out.println((i + 1) + ")" + rankings.get(i));
         }
         System.out.println("\nYour position in global ranking: ");
         //Mostra la posizione nella classica totale salvata sul DB del player
         for (int i = 0; i < globalRanks.size(); i++) {
             if (globalRanks.get(i).getNickname().equals(this.nickname)) {
-                System.out.println((i+1)+")YOU, total prestige points: "+globalRanks.get(i).getScore());
+                System.out.println((i + 1) + ")YOU, total prestige points: " + globalRanks.get(i).getScore());
                 break;
             }
         }
@@ -382,7 +382,7 @@ public class cli implements ui{
     @Override
     public void showAvailableTotems(Set<Totem> totems) {
         System.out.println("Available Totems: ");
-        for(Totem totem: totems) {
+        for (Totem totem : totems) {
             System.out.println("  " + totem.toString());
         }
 
@@ -399,11 +399,14 @@ public class cli implements ui{
     @Override
     public void onGameParticipation() {
         currentState = CliState.TOTEM;
-    private void showGlobalRanks() {
-        System.out.println("\n\n======GLOBAL RANKINGS FOR "+this.numPlayers+" PLAYERS GAMES======");
-        for(int i = 0; i < this.globalRanks.size(); i++) {
-            System.out.println((i+1)+")"+globalRanks.get(i).getNickname()+" - Total Prestige Points: "+globalRanks.get(i).getScore());
-        }
+
     }
 
+    private void showGlobalRanks() {
+        System.out.println("\n\n======GLOBAL RANKINGS FOR " + this.numPlayers + " PLAYERS GAMES======");
+        for (int i = 0; i < this.globalRanks.size(); i++) {
+            System.out.println((i + 1) + ")" + globalRanks.get(i).getNickname() + " - Total Prestige Points: " + globalRanks.get(i).getScore());
+        }
+
+    }
 }
